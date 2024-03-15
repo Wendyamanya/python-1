83085e902b1f6347203aaa8dc933f6f28aca1b07
Wendyamanya <wendixod@gmail.com>
2024-03-15 22:20:59 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,11 @@
+name = 'Wendy Amanya'
+print(name)
+
+age = '23'
+print(age)
+
+location = 'Nakuru'
+print(location)
+
+print ("hello Wendy Amanya, you are 23 years old and live in nakuru")
+       
